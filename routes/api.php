51cfa27f4934ff8d7f6f000d4126cd37e197f56c
Oleg Harmashov <4aro9ei@gmail.com>
2026-06-03 10:52:11 +0300

    lab8

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Blog\PostController;
+use App\Http\Controllers\Api\Blog\Admin\CategoryController;
+use App\Http\Controllers\Api\Blog\Admin\PostController as AdminPostController;
 
 Route::group(['namespace' => 'App\Http\Controllers\Api\Blog', 'prefix' => 'blog'], function () {
     Route::apiResource('posts', PostController::class)->names('blog.posts');
@@ -11,10 +13,14 @@ $groupData = [
     'namespace' => 'App\Http\Controllers\Api\Blog\Admin',
     'prefix' => 'admin/blog',
 ];
+
 Route::group($groupData, function () {
-    //BlogCategory
-    $methods = ['index','store','update',];
+    $methods = ['index', 'store', 'update'];
     Route::apiResource('categories', CategoryController::class)
         ->only($methods)
         ->names('blog.admin.categories');
+
+    Route::apiResource('posts', AdminPostController::class)
+        ->except(['show'])
+        ->names('blog.admin.posts');
 });
